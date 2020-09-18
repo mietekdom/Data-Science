b3d16da99789e7d73982c1e9c2e1c1693de5fb81
mietekdom <mieczyslaw.domansky@gmail.com>
2020-09-18 10:54:48 +0200

    Dashboard

---
+++ 02_dashboard/01_simple_app.py
@@ -0,0 +1,28 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objects as go
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div(children=[
+
+    html.H2(children='Sprzedarz'),
+    dcc.Graph(
+        figure=go.Figure([
+            go.Bar(
+                x=['2017', '2018', '2019', '2020'],
+                y=[150, 180, 230, 100],
+                name='lokalnie'
+            ),
+            go.Bar(
+                x=['2017', '2018', '2019', '2020'],
+                y=[40, 120, 130, 600],
+                name='online'
+            )
+        ])
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
