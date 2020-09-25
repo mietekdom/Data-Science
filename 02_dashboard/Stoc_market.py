0526ae292720a7659264e706f28a96b80f4344cc
mietekdom <mieczyslaw.domansky@gmail.com>
2020-09-25 13:57:22 +0200

    Changes01

---
+++ 02_dashboard/Stoc_market.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# %% importing data
+df = pd.read_csv('./data/ten.csv', index_col=0)
+
+df.columns = ['Open', 'High', 'Low', 'Close', 'Volume']
+
+# %% dzienna procentowa zmiana procentowa w porównaniu z dniem poprzednim
+
+df['Daily_Change'] = df['Close'].pct_change()
+
+# %% zmiana procntowa z okresu 5 dni
+
+df['5_Daily_Change'] = df['Close'].pct_change(periods=5)
+
+# %% zmiana procentowa otwarcia i zmakiecia (sesja giełdowa z dnia)
+
+df['Close_to_Open'] = df[['Open', 'Close']].pct_change(axis=1).drop('Open', axis=1)
+
+# %%
+
+clean_price = df[['Open', 'High', 'Low', 'Close']]
+
+daily_change = clean_price.pct_change()
